0bafc9d54e523b9ef5d7943e5a60a33099c0f783
James Pei <lei.pei@xtalpi.com>
2018-10-23 00:56:13 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Poseidon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poseidon.dir/main.cpp.o"
+  "Poseidon.pdb"
+  "Poseidon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poseidon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
