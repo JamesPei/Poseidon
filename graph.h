585a9ab165acd88c53b1c088d861f1ed6efc9f0d
James Pei <lei.pei@xtalpi.com>
2018-10-28 18:27:37 +0800

    optimize

--- graph.h
+++ graph.h
@@ -7,75 +7,125 @@
 
 #include <vector>
 #include <iostream>
+#include <map>
 
 class Graph{
     /*
-     * 以前向星方式表示图
-     * n:点
-     * m:边
+     * 图的表示方法，包括前向星和邻接集两种方式
      */
 public:
     typedef std::vector<int> VI;
+    int type;
     VI info, next, to;
+    std::map<int, VI> graph;
     Graph(int n=0, int m=0): to(n), next(m){
+        type = 1;
         info.resize(n);
         next.reserve(m);
         to.reserve(m);
     }
 
+    Graph(std::map<int, VI> g){
+        type = 2;
+        graph = g;
+    }
+
     int edge_size(){    //返回边数量
-        return to.size();
+        if(type==1){
+            return to.size();
+        }else{
+            int size=0;
+            std::map<int, VI>::iterator it;
+            for(it = graph.begin(); it!=graph.end(); ++it){
+                size += (it->second).size();
+            }
+            return size;
+        }
     }
 
     int vertex_size(){  //返回值为最大点的编号+1
-        return info.size();
+        if(type==1)
+            return info.size();
+        else
+            return graph.size();
     }
 
+    void add(int i, int j){     //添加一条i到j的边
+        if(type==1){
+            expand(i), expand(j);
+            to.push_back(j);
+            next.push_back(info[i]);
+            info[i] = to.size()-1;
+        }else{
+            graph[i].push_back(j);
+        }
+
+    }
+
+    void del_back(){        // 删除最后一次添加的边
+        if(type==1){
+            int i;
+            for(i=0; i<info.size(); i++)
+                if(info[i]==to.size() - 1){
+                    info[i] = next.back();
+                    //Returns a reference to the last element in the vector. Unlike member vector::end, which returns an
+                    //iterator just past this element, this function returns a direct reference
+                    break;
+                }
+            to.pop_back();
+            next.pop_back();
+        }else{
+            std::cout<<"adjacency list not supported"<<std::endl;
+        }
+    }
+
+    void clear(){           // 清空图类
+        if(type==1){
+            info.clear();
+            next.resize(0);
+            to.resize(0);
+        }else{
+            graph.clear();
+        }
+    }
+
+    void print(){
+        if(type==1) {
+            std::vector<int>::iterator it;
+            std::cout << "info:" << "\n";
+            for (it = info.begin(); it != info.end(); ++it)
+                std::cout << *it << " ";
+            std::cout << std::endl;
+
+            std::cout << "next:" << "\n";
+            for (it = next.begin(); it != next.end(); ++it)
+                std::cout << *it << " ";
+            std::cout << std::endl;
+
+            std::cout << "to:" << "\n";
+            for (it = to.begin(); it != to.end(); ++it)
+                std::cout << *it << " ";
+            std::cout << std::endl;
+        }else{
+            std::map<int, VI>::iterator it;
+            for(it = graph.begin(); it!=graph.end(); ++it){
+                std::cout<<it->first<<":";
+                for(int i:it->second)
+                    std::cout<<i<<" ";
+                std::cout<<std::endl;
+            }
+        }
+    }
+
+private:
     void expand(int i){
         if(info.size() < i+1)
             info.resize(i+1);
     }
-
-    void add(int i, int j){     //添加一条i到j的边
-        expand(i), expand(j);
-        to.push_back(j);
-        next.push_back(info[i]);
-        info[i] = to.size()-1;
-    }
-
-    void del_back(){        // 删除最后一次添加的边
-        int i;
-        for(i=0; i<info.size(); i++)
-            if(info[i]==to.size() - 1){
-                info[i] = next.back();
-                break;
-            }
-        to.pop_back();
-        next.pop_back();
-    }
-
-    void clear(){           // 清空图类
-        info.clear();
-        next.resize(0);
-        to.resize(0);
-    }
-
-    void print(){
-        std::cout<<"info:"<<"\n";
-        for(int i: info)
-            std::cout<<i<<" ";
-        std::cout<<std::endl;
-
-        std::cout<<"next:"<<"\n";
-        for(int i: next)
-            std::cout<<i<<" ";
-        std::cout<<std::endl;
-
-        std::cout<<"to:"<<"\n";
-        for(int i: to)
-            std::cout<<i<<" ";
-        std::cout<<std::endl;
-    }
-
 };
+
+bool topsort();
+void dijkstra();
+void floyd_warshall();
+
 #endif //POSEIDON_GRAPH_H
